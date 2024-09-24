474d43912fccd35ee03bb3e4698d49b91c949a98
Cisneros <ecisneros7@cps.k12.il.us>
2024-09-24 14:36:01 -0500

    first commit

---
+++ python_basics/lesson1.py
@@ -0,0 +1,25 @@
+#mkdir means make directory
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch means create a file
+#rm means remove a file
+# pound sign means comment
+# print("Hello, World!")
+# print("Christmas")
+# print("age \t 25")
+# \t means tab
+# print("address")
+# press control ? to comment out multiple times
+
+# ask for input
+name = input("What is your name? ")
+print("hello, " + name)
+age = input("What is your age?")
+print("You are " + age + " years old")
+address = input("Where do you live?")
+print("You live in " + address)
+school = input("Where do you go to school?")
+print("I go to school at " + school)
+place_of_birth = input("You were born in " + place_of_birth)
+print("Hello, " + name + ". You are " + age + " years old. You were born in " + place_of_birth + ". Your address
---
+++ week_4/mini_challengesWK.py
@@ -0,0 +1,19 @@
+# String Properties Practice #1
+# Concatenate the text "Repetition" 15 times and display the result on the screen.
+# Luckily, you know that strings are multipliable and you can do this activity in a simple and elegant way.
+result = "Repetition " * 15
+print(result)
+# String Properties Practice #2
+# Check if the word "beach" is not found in the following haiku. You should print the boolean.
+# "Whitecaps on the bay:
+# A broken signboard banging
+# In the April wind."
+# — Richard Wright, collected in Haiku: This Other World, 1998
+haiku ="Whitecaps on the bay; A broken signboard banging, In the April wind"
+beach="beach"
+results=beach in haiku
+print(results)
+# String Properties Practice #3
+# Check the Python Documentation to find the description of the len() function. Then, display on the screen the length (in number of characters) of the word electroencephalographist.
+amounts= len("electroencephalographist.")
+print(amounts)
